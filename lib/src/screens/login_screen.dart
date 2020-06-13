9b38853db930c3dbadd966a3498a26d3d6e1db84
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-13 21:14:35 +0300

    submitButton onpressed

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -60,7 +60,9 @@ class LoginScreen extends StatelessWidget {
         return RaisedButton(
           child: Text('Login'),
           color: Colors.blue,
-          onPressed: () {},
+          onPressed: snapshot.hasError ? null : () {
+            print('Ok');
+          },
         );
       },
     );
